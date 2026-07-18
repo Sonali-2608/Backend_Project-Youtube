7320196b02cbf55f329ec081ca811a132e1be880
Sonali-2608 <sonalisingh5469@gmail.com>
2026-07-18 20:44:55 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -16,15 +16,15 @@ app.use(cookieParser())
 
 
 //routes import
-import userRouter from './routes/user.routes.js'
-import healthcheckRouter from "./routes/healthcheck.routes.js"
-import tweetRouter from "./routes/tweet.routes.js"
-import subscriptionRouter from "./routes/subscription.routes.js"
-import videoRouter from "./routes/video.routes.js"
-import commentRouter from "./routes/comment.routes.js"
-import likeRouter from "./routes/like.routes.js"
-import playlistRouter from "./routes/playlist.routes.js"
-import dashboardRouter from "./routes/dashboard.routes.js"
+import userRouter from './routes/user.js'
+import healthcheckRouter from "./routes/healthcheck.js"
+import tweetRouter from "./routes/tweet.js"
+import subscriptionRouter from "./routes/subscription.js"
+import videoRouter from "./routes/video.js"
+import commentRouter from "./routes/comment.js"
+import likeRouter from "./routes/like.js"
+import playlistRouter from "./routes/playlist.js"
+import dashboardRouter from "./routes/dashboard.js"
 
 //routes declaration
 app.use("/api/v1/healthcheck", healthcheckRouter)
